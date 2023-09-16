c932ae912990e85b1d67f96d4025b2e4c6a71322
AndriwRC <randriw4@gmail.com>
2023-09-15 21:22:29 -0500

    Actualizo la conexion a base de datos tipo mysql

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template
-import pyodbc
+import mysql.connector
+
+# Configura los parámetros de conexión
+config = {
+    "user": "public",
+    "password": "42PzVD!K",
+    "host": "localhost",
+    "database": "TasksDB",
+    "port": 3306,
+}
+
 
 def make_connection():
     # Conexion Base de Datos
-    conn = pyodbc.connect(r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\cuc\Desktop\taller-flask\TasksDB.accdb;")
+    conn = mysql.connector.connect(**config)
     # Abrir cursor
-    cursor=conn.cursor()
+    cursor = conn.cursor()
     return conn, cursor
+
 
 def query_db_info(cursor):
     # Consultar informacion de la DB
@@ -14,10 +25,12 @@ def query_db_info(cursor):
     db_info = cursor.fetchall()
     return db_info
 
+
 def close_connection(cursor, conn):
     # Cerrar conexion
-    cursor.close
-    conn.close
+    cursor.close()
+    conn.close()
+
 
 conn, cursor = make_connection()
 db_info = query_db_info(cursor)
@@ -28,12 +41,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index(db_info=db_info):
-    return render_template('index.html', db_info=db_info)
+    return render_template("index.html", db_info=db_info)
+
 
 @app.route("/add/")
 def add_task():
-    return render_template('add.html')
+    return render_template("add.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
-    
